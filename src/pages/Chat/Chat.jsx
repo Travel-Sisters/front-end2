9bd1057209ce2ea4fb1174f73136f04b0504a065
CarlosPossoni <carlos.posson@sptech.school>
2023-11-29 22:19:51 -0300

    axios on chat

--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -1,73 +1,15 @@
-// import React, { useState, useEffect } from 'react';
-// import io from 'socket.io-client';
-// import axios from 'axios';
-// import './Chat.css';
-
-// export default function Chat() {
-//   const [messages, setMessages] = useState([]);
-//   const [newMessage, setNewMessage] = useState('');
-//   const socket = io('http://localhost:5173');
-
-//   useEffect(() => {
-//     socket.on('message', (message) => {
-//       setMessages((prevMessages) => [...prevMessages, message]);
-//     });
-
-//     axios.get('http://seu-servidor/api/messages')
-//       .then((response) => {
-//         setMessages(response.data);
-//       });
-
-//     return () => {
-//       socket.disconnect();
-//     };
-//   }, [socket]);
-
-//   const sendMessage = () => {
-//     socket.emit('message', newMessage);
-//     axios.post('http://seu-servidor/api/messages', { message: newMessage });
-//     setNewMessage('');
-//   }
-
-//   return (
-//     <div id="page-chat">
-//       <div id="app">
-//         <div id="messages">
-//           <div id="last-seen">hoje 11:30</div>
-//           <div className="messages">
-//             {messages.map((message, index) => (
-//               <div key={index} className={`message ${message.from === 'you' ? 'you' : ''}`}>
-//                 <div className="top">{message.sender} - {message.time}</div>
-//                 <div className="body">{message.body}</div>
-//               </div>
-//             ))}
-//           </div>
-//         </div>
-//         <form id="bottom" onSubmit={(e) => { e.preventDefault(); sendMessage(); }}>
-//           <input
-//             type="text"
-//             placeholder="digite sua mensagem"
-//             value={newMessage}
-//             onChange={(e) => setNewMessage(e.target.value)}
-//           />
-//           <button type="submit">
-//             <i className="ph-paper-plane-right-fill"></i>
-//           </button>
-//         </form>
-//       </div>
-//     </div>
-//   );
-// }
-
 import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
-import './Chat.css'; // Importe o arquivo CSS aqui
-const ChatRoom = () => {
+import axios from 'axios';
+import './Chat.css';
+
+export default function Chat() {
+  // const [messages, setMessages] = useState([]);
+  // const [newMessage, setNewMessage] = useState('');
+  // const socket = io('http://localhost:3001');
   const [socket, setSocket] = useState(null);
   const [message, setMessage] = useState('');
-  const [chatId, setChatId] = useState(); // Simula um chat específico
-
-
+  const [chatId, setChatId] = useState();
 
   useEffect(() => {
     const newSocket = io('http://localhost:3001');
@@ -76,8 +18,41 @@ const ChatRoom = () => {
     return () => newSocket.close();
   }, []);
 
+  const getChatViagem = () => {
+    axios.get(`http://localhost:8080/`)
+  }
+
+  useEffect(() => {
+    socket.on('message', (message) => {
+      setMessages((prevMessages) => [...prevMessages, message]);
+    });
+
+    
+
+    return () => {
+      socket.disconnect();
+    };
+  }, [socket]);
+
+  const sendMessage = () => {
+    socket.emit('message', newMessage);
+    const [currentDate, setCurrentDate] = useState(new Date());
+
+    axios.post('http://localhost:8080/chat/', {id: sessionStorage.getItem('idUsuarioLogin'),
+                                               mensagem: newMessage,
+                                               data: currentDate.getDate,
+                                               hora: currentDate.getTime,
+                                               //chat_viagem: ->preciso terminar
+
+    })
+      .then((response) => {
+        setMessages(response.data);
+      });
+    setNewMessage('');
+  }
+ 
   const handleChangeChat = (newChatId) => {
-    setChatId(newChatId); // Atualiza o estado do chatId com o novo valor
+    setChatId(newChatId); 
   };
 
   const handleJoinChat = () => {
@@ -89,35 +64,60 @@ const ChatRoom = () => {
     socket.emit('message', { message, chatId });
     setMessage('');
   };
-
   useEffect(() => {
     if (!socket) return;
 
     socket.on('new_message', (data) => {
       console.log(`Nova mensagem: ${data.message} - Usuário: ${data.user}`);
-      // Lógica para exibir a mensagem recebida na interface
     });
 
     return () => socket.off('new_message');
   }, [socket]);
 
   return (
-    <div>
-      <button onClick={() => handleChangeChat(1)}>Mudar para Chat 1</button>
-      <br />
-      <button onClick={() => handleChangeChat(2)}>Mudar para Chat 2</button>
-      <br />
-      <button onClick={handleJoinChat}>Entrar no Chat</button>
-      <br />
-      <button onClick={handleJoinChat}>Entrar em outro Chat</button>
-      <input
-        type="text"
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
-      />
-      <button onClick={handleMessageSend}>Enviar Mensagem</button>
+    <div id="page-chat">
+      <div id="app">
+        <div id="messages">
+          <div id="last-seen">hoje 11:30</div>
+          <div className="messages">
+            {messages.map((message, index) => (
+              <div key={index} className={`message ${message.from === 'you' ? 'you' : ''}`}>
+                <div className="top">{message.sender} - {message.time}</div>
+                <div className="body">{message.body}</div>
+              </div>
+            ))}
+          </div>
+        </div>
+        <form id="bottom" onSubmit={(e) => { e.preventDefault(); sendMessage(); }}>
+          <input
+            type="text"
+            placeholder="digite sua mensagem"
+            value={newMessage}
+            onChange={(e) => setNewMessage(e.target.value)}
+          />
+          <button type="submit">
+            <i className="ph-paper-plane-right-fill"></i>
+          </button>
+        </form>
+      </div>
     </div>
   );
-};
+}
 
-export default ChatRoom;
+// return (
+//   <div>
+//     <button onClick={() => handleChangeChat(1)}>Mudar para Chat 1</button>
+//     <br />
+//     <button onClick={() => handleChangeChat(2)}>Mudar para Chat 2</button>
+//     <br />
+//     <button onClick={handleJoinChat}>Entrar no Chat</button>
+//     <br />
+//     <button onClick={handleJoinChat}>Entrar em outro Chat</button>
+//     <input
+//       type="text"
+//       value={message}
+//       onChange={(e) => setMessage(e.target.value)}
+//     />
+//     <button onClick={handleMessageSend}>Enviar Mensagem</button>
+//   </div>
+// );
